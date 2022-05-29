cb33e7413295f22515d223a322dda319439cfc06
Diana Kariuki <dianakariuki842@gmail.com>
2022-05-29 09:18:02 +0300

    This commit imports a http response in views

--- album/views.py
+++ album/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Image, Category, Location
+from django.http import HttpResponse, Http404
 
-# Create your views here.
+def welcome(request):
+    categories = Category.objects.all()
+    locations = Location.objects.all()
+    return render(request, 'categories.html',{"categories":categories,"locations":locations})
+
