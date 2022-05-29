539c79da33ceb32e67c44eecd97ebb9af4702a48
Diana Kariuki <dianakariuki842@gmail.com>
2022-05-29 09:19:24 +0300

    This commit creates a view for the categories

--- album/views.py
+++ album/views.py
@@ -7,3 +7,11 @@ def welcome(request):
     locations = Location.objects.all()
     return render(request, 'categories.html',{"categories":categories,"locations":locations})
 
+def categories(request):
+    categories = Category.objects.all()
+    locations = Location.objects.all()
+    images = Image.objects.all()
+    return render(request, 'categories.html',{"categories":categories,"images":images,"locations":locations})
+
+
+
