a70be7cb994a018035776a8c4384201385cb0eae
Diana Kariuki <dianakariuki842@gmail.com>
2022-05-30 00:30:56 +0300

    This commit updates on the url

--- album/urls.py
+++ album/urls.py
@@ -8,6 +8,7 @@ urlpatterns=[
     re_path(r'^categories$',views.categories,name = 'categories'),
     re_path(r'^category$', views.category, name='category_results'),
     re_path(r'^location/(\d+)',views.location,name = 'location'),
+    
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
